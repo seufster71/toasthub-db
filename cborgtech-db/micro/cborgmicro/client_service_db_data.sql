9c4cbcae321d8fe2428278098d1daaff3651b0b5
Edward Seufert <seufster@gmail.com>
2022-02-12 11:51:19 -0500

    update service names

--- cborgtech-db/micro/cborgmicro/client_service_db_data.sql
+++ cborgtech-db/micro/cborgmicro/client_service_db_data.sql
@@ -2,26 +2,26 @@
 -- service classes
 -- admin
 INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_SVC','1.0','1.0','AdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('MENU_SVC','1.0','1.0','MenuAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('PREF_PAGE_SVC','1.0','1.0','PrefPageAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('FORM_SVC','1.0','1.0','FormAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('LANGUAGE_SVC','1.0','1.0','LanguageAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('USERS_SVC','1.0','1.0','UsersAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('MAIL_SVC','1.0','1.0','MailSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('OPTIONS_SVC','1.0','1.0','PrefOptionSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('LOGOUT_SVC','1.0','1.0','LogOutSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('CATEGORY_SVC','1.0','1.0','CategoryAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('STATUS_SVC','1.0','1.0','StatusAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('WORKFLOW_SVC','1.0','1.0','WorkFlowSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ROLES_SVC','1.0','1.0','RoleAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('PERMISSIONS_SVC','1.0','1.0','PermissionAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('PREF_FORMFIELD_SVC','1.0','1.0','PrefFormFieldAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('PREF_LABEL_SVC','1.0','1.0','PrefLabelAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('PREF_OPTION_SVC','1.0','1.0','PrefOptionAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('PREF_TEXT_SVC','1.0','1.0','PrefTextAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('SERVICE_CRAWLER_SVC','1.0','1.0','ServiceCrawlerAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('CLIENT_DOMAIN_SVC','1.0','1.0','ClientDomainAdminSvc','ADMIN','LOCAL',null);
-INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('APPLICATION_SVC','1.0','1.0','ApplicationAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_MENU_SVC','1.0','1.0','MenuAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_PREF_PAGE_SVC','1.0','1.0','PrefPageAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_FORM_SVC','1.0','1.0','FormAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_LANGUAGE_SVC','1.0','1.0','LanguageAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_USER_SVC','1.0','1.0','UsersAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_MAIL_SVC','1.0','1.0','MailSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_OPTION_SVC','1.0','1.0','PrefOptionSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_LOGOUT_SVC','1.0','1.0','LogOutSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_CATEGORY_SVC','1.0','1.0','CategoryAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_STATUS_SVC','1.0','1.0','StatusAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_WORKFLOW_SVC','1.0','1.0','WorkFlowSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_ROLE_SVC','1.0','1.0','RoleAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_PERMISSION_SVC','1.0','1.0','PermissionAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_PREF_FORMFIELD_SVC','1.0','1.0','PrefFormFieldAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_PREF_LABEL_SVC','1.0','1.0','PrefLabelAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_PREF_OPTION_SVC','1.0','1.0','PrefOptionAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_PREF_TEXT_SVC','1.0','1.0','PrefTextAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_SERVICE_CRAWLER_SVC','1.0','1.0','ServiceCrawlerAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_CLIENT_DOMAIN_SVC','1.0','1.0','ClientDomainAdminSvc','ADMIN','LOCAL',null);
+INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('ADMIN_APPLICATION_SVC','1.0','1.0','ApplicationAdminSvc','ADMIN','LOCAL',null);
 
 -- public
 INSERT INTO service_class (service_name,api_version,app_version,class_name,category,location,created) VALUES ('PUBLIC_SVC','1.0','1.0','PublicSvc','PUBLIC','LOCAL',null);
